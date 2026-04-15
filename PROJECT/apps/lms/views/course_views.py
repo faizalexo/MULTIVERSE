44562fd77a07f46bc4b8224638a59264039bfa06
KAXXONBYTE <Fazelk504@gmail.com>
2026-04-15 14:49:08 +0530

    feat

--- PROJECT/apps/lms/views/course_views.py
+++ PROJECT/apps/lms/views/course_views.py
@@ -1,32 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from ..models import Course
+from django.shortcuts import render, get_object_or_404
+from apps.lms.models import Course
 
-# 📚 Course List
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'lms/course_list.html', {'courses': courses})
 
-
-# 📖 Course Detail
 def course_detail(request, id):
     course = get_object_or_404(Course, id=id)
     return render(request, 'lms/course_detail.html', {'course': course})
-
-
-# ➕ Add Course
-def add_course(request):
-    if request.method == "POST":
-        Course.objects.create(
-            title=request.POST['title'],
-            description=request.POST['description'],
-            instructor=request.POST['instructor']
-        )
-        return redirect('course_list')
-
-    return render(request, 'lms/add_course.html')
-
-
-# 👤 About Page
-def about(request):
-    return render(request, 'lms/about.html')
-
